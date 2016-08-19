7399900a14509a4cf54309dd52117160b75eee24
David T. Kofahl <david.kofahl@mediastorm.com>
2016-08-19 12:08:12 -0700

    imagecpations plugin

--- lib/rich.rb
+++ lib/rich.rb
@@ -67,7 +67,7 @@ module Rich
   @@editor = {
     :height => 400,
     :stylesSet  =>  [],
-    :extraPlugins => 'stylesheetparser,richfile,mediaembed,showblocks',
+    :extraPlugins => 'stylesheetparser,richfile,mediaembed,showblocks,imageCaptions',
     :removePlugins => 'scayt,image,forms',
     :contentsCss => :default,
     :removeDialogTabs => 'link:advanced;link:target',
